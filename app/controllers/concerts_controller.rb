eb7301d20e2ee62d4451b8c7158e3f21c65b6f6f
Juan Lopez <capjuancode@gmail.com>
2024-12-26 12:43:39 -0500

    working controller show with paths id

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -1,9 +1,16 @@
 class ConcertsController < ApplicationController
+  before_action :set_event, only: :show
+
   def index
   end
 
   def show
-    event = Event.includes(:ticket_types).first
-    render inertia: 'BandForm', props: { event: event, ticket_types: event.ticket_types }
+    render inertia: 'BandForm', props: { event: @event, ticket_types: @event.ticket_types }
+  end
+
+  private
+
+  def set_event
+    @event = Event.includes(:ticket_types).find(params['id'])
   end
 end
